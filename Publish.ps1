4550f82301587f86cc7e6bb41d876297f4f29cb2
cole.wintringham@gmail.com <cole.wintringham@gmail.com>
2024-03-16 09:33:38 -0700

    main

--- Publish.ps1
+++ Publish.ps1
@@ -163,7 +163,7 @@ param (
 
     ## => User Config <= ## 
     $ProjectPath = "P3R.bgm.MakotoMP3.battlethemes.Template.csproj",
-    $PackageName = "P3R.bgm.MakotoMP3.battlethemes",
+    $PackageName = "P3R.bgm.MakotoMP3.battlethemes.special",
     $PublishOutputDir = "Publish/ToUpload",
 
     ## => User: Delta Config
